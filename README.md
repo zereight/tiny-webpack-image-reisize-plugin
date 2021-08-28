0a141cb08b0259f9b53b813ba075ccbb6a350ad0
zereight <fullstackmachinelearning@gmail.com>
2021-08-28 18:44:49 +0900

    feat: 플러그인 기능 구현

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 이미지들을 webp로 변환해주는 webapck plugin
 
 누구나 따라할 수 있게 쉽게 구현
+
+## Support
+
+Only webpack 5
--- src/index.js
+++ src/index.js
@@ -1,15 +1,72 @@
 const imagemin = require("imagemin");
 const imageminWebp = require("imagemin-webp");
-const imageminGif2Webp = require("imagemin-gif2webp");
+
+const PLUGIN_NAME = "WebpConvertPlugin";
+const IMAGES_POSTFIX = /\.(jpe?g|png|gif)/;
 
 class WebpConvertPlugin {
-  constructor({}) {}
-
   apply(compiler) {
-    const onEmit = (compilation, callback) => {
-      console.log(compilation.getAssets());
-      let assertNames = Object.keys(compilation.getAssets());
-    };
+    compiler.hooks.thisCompilation.tap(PLUGIN_NAME, (compilation) => {
+      compilation.hooks.processAssets.tapAsync(
+        {
+          name: PLUGIN_NAME,
+          stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+        },
+        (assets, callback) => {
+          const assetFileNames = Object.keys(assets);
+
+          const processImageminForAssetFileAsyncs = assetFileNames.map(
+            (assetFileName) => {
+              if (!IMAGES_POSTFIX.test(assetFileName)) return;
+
+              const removedPostFixName = assetFileName
+                .split(".")
+                .slice(0, -1)
+                .join(".");
+              const convertedFileName = `${removedPostFixName}.webp`;
+
+              const assetFile = compilation.assets[assetFileName];
+
+              const processImageminAsync = imagemin
+                .buffer(assetFile.source(), {
+                  plugins: [
+                    imageminWebp({
+                      quality: 0,
+                      size: 200,
+                      resize: {
+                        width: 1920,
+                        height: 1080,
+                      },
+                    }),
+                  ],
+                })
+                .then((processedBuffer) => {
+                  const size =
+                    (assetFile.size() - processedBuffer.length) / 1024;
+
+                  delete compilation.assets[assetFileName];
+
+                  compilation.emitAsset(convertedFileName, {
+                    source: () => processedBuffer,
+                    size: () => processedBuffer.length,
+                  });
+
+                  return size;
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+
+              return processImageminAsync;
+            }
+          );
+
+          Promise.allSettled(processImageminForAssetFileAsyncs).then(() => {
+            callback();
+          });
+        }
+      );
+    });
   }
 }
 
